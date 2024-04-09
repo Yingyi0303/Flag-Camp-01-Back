334c4409926b0c14d5d96b32fe409e44f4bed0c2
ArthurYangCN <shihuayang02@gmail.com>
2024-04-09 14:30:34 -0700

    login

---
+++ Loginout/src/main/resources/database-init.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password VARCHAR(100) NOT NULL,
+    enabled  TINYINT      NOT NULL DEFAULT 1
+);
+
+
+CREATE TABLE authorities
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
